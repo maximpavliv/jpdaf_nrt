512aef430b2ba11838856a949a5d5dcf466836c3
maximpavliv <maxim.pavliv@epfl.ch>
2020-01-23 11:02:31 -0500

    first commit. Max missed and min acceptance are now read from params, but P_init and Q are still computed in Kalman

---
+++ build/CMakeFiles/jpdaf_tracker.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/arpl/jpdaf_tracking/src/global_tracker.cpp" "/home/arpl/jpdaf_tracking/build/CMakeFiles/jpdaf_tracker.dir/src/global_tracker.cpp.o"
+  "/home/arpl/jpdaf_tracking/src/hungarianAlg.cpp" "/home/arpl/jpdaf_tracking/build/CMakeFiles/jpdaf_tracker.dir/src/hungarianAlg.cpp.o"
+  "/home/arpl/jpdaf_tracking/src/imagemanager.cpp" "/home/arpl/jpdaf_tracking/build/CMakeFiles/jpdaf_tracker.dir/src/imagemanager.cpp.o"
+  "/home/arpl/jpdaf_tracking/src/kalman.cpp" "/home/arpl/jpdaf_tracking/build/CMakeFiles/jpdaf_tracker.dir/src/kalman.cpp.o"
+  "/home/arpl/jpdaf_tracking/src/local_tracker.cpp" "/home/arpl/jpdaf_tracking/build/CMakeFiles/jpdaf_tracker.dir/src/local_tracker.cpp.o"
+  "/home/arpl/jpdaf_tracking/src/main.cpp" "/home/arpl/jpdaf_tracking/build/CMakeFiles/jpdaf_tracker.dir/src/main.cpp.o"
+  "/home/arpl/jpdaf_tracking/src/track.cpp" "/home/arpl/jpdaf_tracking/build/CMakeFiles/jpdaf_tracker.dir/src/track.cpp.o"
+  "/home/arpl/jpdaf_tracking/src/tracker.cpp" "/home/arpl/jpdaf_tracking/build/CMakeFiles/jpdaf_tracker.dir/src/tracker.cpp.o"
+  "/home/arpl/jpdaf_tracking/src/tracker_param.cpp" "/home/arpl/jpdaf_tracking/build/CMakeFiles/jpdaf_tracker.dir/src/tracker_param.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "/usr/include/opencv"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
